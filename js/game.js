d29feba90d17a7f52e66cb3791fb93ada24fbbb7
Fredrik <32198607+haardes@users.noreply.github.com>
2019-03-15 10:57:08 +0100

    Animations fixed

--- js/game.js
+++ js/game.js
@@ -1,71 +1,137 @@
 let stage = "index";
+let prevStage = "";
+
+function toggleActive(e) {
+    e.classList.contains("active") ? e.classList.remove("active") : e.classList.add("active");
+}
 
 function startGame() {
-    fadeHeaderIn();
-    fadeIndexOut();
+    if (stage === "index") {
+        fadeIndexOut();
+        fadeHeaderIn();
+        setTimeout(() => {
+            fadeSetupIn();
+        }, 1000);
+        stage = "setup";
+    } else if (stage === "setup") {
+
+    }
 }
 
 function back() {
-    console.log("Going back one 'level'");
-    fadeHeaderOut();
-    fadeSetupOut(false);
-}
-
-function fadeIndexIn() {
-    console.log("Fade index in!");
-    let indexContainer = document.querySelector(".index-container");
-    indexContainer.style.WebkitAnimation = "fadeIn 1s forwards";
-    indexContainer.classList.remove("hidden");
-}
-
-function fadeIndexOut() {
-    let indexContainer = document.querySelector(".index-container");
-    indexContainer.style.WebkitAnimation = "fadeOut 1s forwards";
-    indexContainer.addEventListener("webkitAnimationEnd", () => {
-        console.log("fadeIndexOut ended");
-        if (stage === "index") {
-            document.querySelector(".start-button").classList.add("hidden");
-            document.querySelector(".index-container .how-to-button").classList.add("hidden");
-            indexContainer.classList.add("hidden");
-            stage = "setup";
-            fadeSetupIn();
-        }
-    })
-}
-
-function fadeSetupIn() {
-    let setupContainer = document.querySelector(".setup-container");
-    setupContainer.classList.remove("hidden");
-    setupContainer.style.WebkitAnimation = "fadeIn 1s forwards";
-}
-
-function fadeSetupOut(next) {
-    let setupContainer = document.querySelector(".setup-container");
-    setupContainer.style.WebkitAnimation = "fadeOut 1s forwards";
-    setupContainer.addEventListener("webkitAnimationEnd", () => {
-        setupContainer.classList.add("hidden");
-        if (!next) {
-            console.log("Doing fade index in");
+    if (stage === "setup") {
+        fadeSetupOut();
+        fadeHeaderOut();
+        setTimeout(() => {
             fadeIndexIn();
+        }, 1000);
+        stage = "index";
+    } else if (stage === "help") {
+        if (prevStage === "index") {
+            fadeHeaderOut();
+            fadeHelpOut();
+            setTimeout(() => {
+                fadeIndexIn();
+            }, 1000);
+            stage = "index";
+        } else if (prevStage === "setup") {
+            fadeHelpOut();
+            setTimeout(() => {
+                fadeSetupIn();
+            }, 1000);
+            stage = "setup";
         }
-    })
+    } else if (stage === "playing") {
+        stage = "setup";
+    }
+}
+
+function help() {
+    if (stage === "index") {
+        fadeIndexOut();
+        fadeHeaderIn();
+        setTimeout(() => {
+            fadeHelpIn();
+        }, 1000);
+        stage = "help";
+        prevStage = "index";
+    } else if (stage === "setup") {
+        fadeSetupOut();
+        setTimeout(() => {
+            fadeHelpIn();
+        }, 1000);
+        stage = "help";
+        prevStage = "setup";
+    } else if (stage === "playing") {
+        stage = "help";
+        prevStage = "playing";
+    }
 }
 
 function fadeHeaderIn() {
     let headerButtons = document.querySelectorAll(".header>button");
     headerButtons.forEach(button => {
-        button.classList.remove("hidden");
-        button.style.WebkitAnimation = "fadeIn 1s forwards";
+        toggleActive(button);
     });
 }
 
 function fadeHeaderOut() {
     let headerButtons = document.querySelectorAll(".header>button");
     headerButtons.forEach(button => {
-        button.style.WebkitAnimation = "fadeOut 1s forwards";
-        button.addEventListener("webkitAnimationEnd", () => {
-            button.style.WebkitAnimation = "";
-            button.classList.add("hidden");
-        })
+        toggleActive(button);
     });
 }
+
+function fadeIndexIn() {
+    let index = document.querySelector(".index-container");
+    toggleActive(document.querySelector(".index-container"));
+}
+
+function fadeIndexOut() {
+    let index = document.querySelector(".index-container");
+    toggleActive(document.querySelector(".index-container"));
+}
+
+function fadeSetupIn() {
+    let setup = document.querySelector(".setup-container");
+    toggleActive(document.querySelector(".setup-container"));
+}
+
+function fadeSetupOut() {
+    let setup = document.querySelector(".setup-container");
+    toggleActive(document.querySelector(".setup-container"));
+}
+
+function fadeHelpIn() {
+    toggleActive(document.querySelector(".help-container"));
+    let helpEls = document.querySelectorAll(".help-container>p");
+    helpEls.forEach(el => {
+        toggleActive(el);
+    })
+}
+
+function fadeHelpOut() {
+    let helpEls = document.querySelectorAll(".help-container>p");
+    helpEls.forEach(el => {
+        toggleActive(el);
+    })
+    setTimeout(function () {
+        toggleActive(document.querySelector(".help-container"));
+    }, 1000);
+}
+
+window.onload = function () {
+    fadeIndexIn();
+}
+
+document.addEventListener("animationstart", (e) => {
+    if (e.animationName === 'fade-in' || e.animationName === 'help-in') {
+        e.target.classList.add('did-fade-in');
+    }
+});
+
+document.addEventListener('animationend', function (e) {
+    if (e.animationName === 'fade-out' || e.animationName === 'help-out') {
+        e.target.classList.remove('did-fade-in');
+    }
+});
